6fd8435db817ef11a67da74b6ec6e6118d515d0b
jwilliams0496 <jcwilliams0496@gmail.com>
2018-05-11 03:16:36 -0400

    Make it go faster

--- src/searchCounty.js
+++ src/searchCounty.js
@@ -1,78 +1,35 @@
 // Globally defined, contains the csv data in JSON (see console log)
-var incomeJSON;
-var propertyJSON;
-var livingJSON;
-var rentJSON;
-var climateJSON;
+var everythingJSON;
 
 // Array with county list
 var countyList = [];
 
 function parseCountyData() {
     // Papa Parse Median Income Values
-    Papa.parse(incomeData,
+    Papa.parse(everythingData,
         {
             header: true,
             dynmicTyping: true,
             complete: function (results) {
-                incomeJSON = JSON.parse(JSON.stringify(results.data));
+                everythingJSON = JSON.parse(JSON.stringify(results.data));
             }
         });
 
-    // Papa Parse Median Property Values
-    Papa.parse(propertyData,
-        {
-            header: true,
-            dynamicTyping: true,
-            complete: function (results) {
-                propertyJSON = JSON.parse(JSON.stringify(results.data));
-            }
-        });
-
-    // Papa Parse Cost of Living Values
-    Papa.parse(livingData,
-        {
-            header: true,
-            dynamicTyping: true,
-            complete: function (results) {
-                livingJSON = JSON.parse(JSON.stringify(results.data));
-            }
-        });
-
-    // Papa Parse 50th percentile Rent Values
-    Papa.parse(rentData,
-        {
-            header: true,
-            dynamicTyping: true,
-            complete: function (results) {
-                rentJSON = JSON.parse(JSON.stringify(results.data));
-            }
-        });
-
-    // Papa Parse 50th percentile Rent Values
-    Papa.parse(climateData,
-        {
-            header: true,
-            dynamicTyping: true,
-            complete: function (results) {
-                climateJSON = JSON.parse(JSON.stringify(results.data));
-            }
-        });
 
     // Push all county names into one list
-    for (var key = 0; key < propertyJSON.length; key++) {
+    for (var key = 0; key < everythingJSON.length; key++) {
         countyList.push({
-            state: propertyJSON[key].geo_name.substr(-2),
-            areaName: propertyJSON[key].geo_name,
-            countyName: propertyJSON[key].geo_name.slice(0, -4)
+            state: everythingJSON[key].state,
+            areaName: everythingJSON[key].areaName,
+            countyName: everythingJSON[key].countyName,
+            geoID: everythingJSON[key].geoID
         });
     }
 
 }
 
-function searchCounty(countyToSearch, stateToSearch) {
+function searchCounty(countyToSearch) {
 
-    var areaToSearch = countyToSearch + ", " + stateToSearch;
     var resultsJSON;
 
     // Reset Variables
@@ -87,58 +44,38 @@ function searchCounty(countyToSearch, stateToSearch) {
     var costOfLiving = null;
     var costOfGroceries = null;
     var fips2010 = null;
+    var fipsTxt = null;
     var hu2010 = null;
     var population = null;
     var geoID = null;
     var jan = null;
     var july = null;
+    var lat = null;
+    var long = null;
 
     // Find area name in rentJSON
-    for (var key = 0; key < rentJSON.length; key++) {
-        if (angular.lowercase(areaToSearch) === angular.lowercase(rentJSON[key].cntyname + ", " + rentJSON[key].state_alpha)) {
-            state = rentJSON[key].state_alpha;
-            areaName = rentJSON[key].cntyname + ", " + rentJSON[key].state_alpha;
-            countyName = countyToSearch;
-            rent1bed = rentJSON[key].rent50_1;
-            rent4bed = rentJSON[key].rent50_4;
-
-            fips2010 = rentJSON[key].fips2010;
-            hu2010 = rentJSON[key].hu2010;
-            population = rentJSON[key].pop2010;
-        }
-    }
-
-    // match the name found in rentJSON with information in incomeJSON
-    for (var key = 0; key < incomeJSON.length; key++) {
-        if (areaName === incomeJSON[key].Area_name || countyName === incomeJSON[key].Area_name) {
-            medianHHIncome = incomeJSON[key].Median_Household_Income_2016;
-
-            fipsTxt = incomeJSON[key].FIPStxt;
-        }
-    }
-
-    // match the name found in the rentJSON with the information in propertyJSON
-    for (var key = 0; key < propertyJSON.length; key++) {
-        if (areaName === propertyJSON[key].geo_name) {
-            medianProperty = propertyJSON[key].Median_Property_Value_2016;
-
-            geoID = propertyJSON[key].geo_id;
-        }
-    }
-
-    // Loop through states in living JSON until current state is found
-    for (var key in livingJSON) {
-        if (livingJSON[key].State === state) {
-            costOfLiving = livingJSON[key].Index;
-            costOfGroceries = livingJSON[key].Grocery;
-        }
-    }
-
-    // match the geo id found in the climateJSON with the information in climateJSON
-    for (var key = 0; key < climateJSON.length; key++) {
-        if (geoID === (climateJSON[key].GEO_ID)) {
-            jan = climateJSON[key].Jan;
-            july = climateJSON[key].July;
+    for (var key = 0; key < everythingJSON.length; key++) {
+        if (countyToSearch === everythingJSON[key].geoID) {
+            state = everythingJSON[key].state;
+            countyName = everythingJSON[key].countyName;
+            areaName = everythingJSON[key].areaName;
+            medianHHIncome = everythingJSON[key].medianHHIncome;
+            medianProperty = everythingJSON[key].medianProperty;
+            propName = everythingJSON[key].propName;
+            rent1bed = everythingJSON[key].rent1Bed;
+            rent4bed = everythingJSON[key].rent4bed;
+            costOfLiving = everythingJSON[key].costOfLiving;
+            costOfGroceries = everythingJSON[key].costOfGroceries;
+            fips2010 = everythingJSON[key].fips2010;
+            fipsTxt = everythingJSON[key].fipsTxt;
+            hu2010 = everythingJSON[key].hu2010;
+            population = everythingJSON[key].population;
+            geoID = everythingJSON[key].geoID;
+            jan = everythingJSON[key].jan;
+            july = everythingJSON[key].july;
+            lat = everythingJSON[key].lat;
+            long = everythingJSON[key].long;
+            break;
         }
     }
 
@@ -158,7 +95,9 @@ function searchCounty(countyToSearch, stateToSearch) {
         fips2010: fips2010,
         fipsTxt: fipsTxt,
         jan: jan,
-        july: july
+        july: july,
+        lat: lat,
+        long: long
     };
 
     return resultsJSON;
